adfd74c514e0fae3f7905a7081d08b72a8ea3985
Bruno Marques <brunomcamilo.br@gmail.com>
2024-07-01 23:55:59 -0300

    Recuperando a porta da instância em execução

--- src/main/java/br/com/microservices/cambioservice/controller/CambioController.java
+++ src/main/java/br/com/microservices/cambioservice/controller/CambioController.java
@@ -1,6 +1,8 @@
 package br.com.microservices.cambioservice.controller;
 
 import br.com.microservices.cambioservice.model.Cambio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,13 @@ import java.math.BigDecimal;
 @RequestMapping("cambio-service")
 public class CambioController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/cambio/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable String amount, @PathVariable String from, @PathVariable String to) {
-        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+        //recuperando a porta da aplicação
+        var port = environment.getProperty("local.server.port");
+        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
     }
 }
